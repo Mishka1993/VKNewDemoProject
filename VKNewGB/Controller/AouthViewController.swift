07aee06a8bb17e9bc5b4c84e5f750265453b5b70
Mikhail <misha220993@gmail.com>
2022-03-31 09:42:02 +0400

    Initial project

---
+++ VKNewGB/Controller/AouthViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKNewGB
+//
+//  Created by Михаил Киржнер on 30.03.2022.
+//
+
+import UIKit
+
+class AouthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
